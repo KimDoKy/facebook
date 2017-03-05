db439a24425b97bc1b26faacdbe2369f31861118
Kim DoKy <makingfunk0@gmail.com>
2017-03-05 21:21:59 +0900

    Facebook - login 다이얼로그 이후 엑서스 토큰을 받고 검증 구현

--- django_app/member/urls.py
+++ django_app/member/urls.py
@@ -4,6 +4,6 @@ from member import views
 
 app_name = 'member'
 urlpatterns = [
-    url(r'^login/', views.login_fbv, name='login'),
+    url(r'^login/$', views.login_fbv, name='login'),
     url(r'^login/facebook/$', views.login_facebook, name='login_facebook'),
 ]
--- django_app/member/views.py
+++ django_app/member/views.py
@@ -1,5 +1,10 @@
-from solfacebook import settings
+from pprint import pprint
+
+import requests
 from django.shortcuts import render
+
+from solfacebook import settings
+
 
 def login_fbv(request):
     facebook_app_id = settings.config['facebook']['app_id']
@@ -8,5 +13,38 @@ def login_fbv(request):
     }
     return render(request, 'member/login.html', context)
 
+
 def login_facebook(request):
-    print(request.GET)
+    APP_ID = settings.config['facebook']['app_id']
+    SECRET_CODE = settings.config['facebook']['secret_code']
+    REDIRECT_URI = 'http://localhost:8000/member/login/facebook/'
+    APP_ACCESS_TOKEN = '{app_id}|{secret_code}'.format(
+        app_id=APP_ID,
+        secret_code=SECRET_CODE
+    )
+
+    if request.GET.get('code'):
+        code = request.GET.get('code')
+
+        url_request_access_token = 'https://graph.facebook.com/v2.8/oauth/access_token'
+        params = {
+            'client_id': APP_ID,
+            'redirect_uri': REDIRECT_URI,
+            'client_secret': SECRET_CODE,
+            'code': code,
+        }
+        r = requests.get(url_request_access_token, params=params)
+        dict_access_token = r.json()
+        USER_ACCESS_TOKEN = dict_access_token['access_token']
+        print('ACCESS_TOKEN : %s' % USER_ACCESS_TOKEN)
+
+        url_debug_token = 'https://graph.facebook.com/debug_token'
+        params = {
+            'input_token': USER_ACCESS_TOKEN,
+            'access_token': APP_ACCESS_TOKEN,
+        }
+        r = requests.get(url_debug_token, params=params)
+        dict_debug_token = r.json()
+        pprint(dict_debug_token)
+        USER_ID = dict_debug_token['data']['user_id']
+        print('USER_ID : %s' % USER_ID)
